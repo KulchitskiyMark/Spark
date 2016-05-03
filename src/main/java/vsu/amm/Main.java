88dde133dd695d648c8611f1c0f15dfab961fa0b
Mark Kulchitskiy <mark367@bk.ru>
2016-05-03 14:59:04 +0300

    added top-rated films (for each age group of viewers, by genre, among all)

--- src/main/java/vsu/amm/Main.java
+++ src/main/java/vsu/amm/Main.java
@@ -5,10 +5,10 @@ public class Main
     public static void main(String[] args)
     {
         SparkComputing job = new SparkComputing();
-        //job.topGenres();
-        //job.topRatedFilms();
-        //job.topRatedFilmsGender();
-        //job.topDiscussedFilms();
+        job.topGenres();
+        job.topRatedFilms();
+        job.topRatedFilmsGender();
+        job.topDiscussedFilms();
         job.topRatedGroup();
     }
 }
